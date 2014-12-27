b8764b56592d38b92ec1fc708826e01b749c693c
gitccsand <gozilla@163.com>
2014-12-27 18:31:17 +0800

    init ds-workspace 20141227

---
+++ simprotocol/src/com/http/testsign/SignatureConsumer.java
@@ -0,0 +1,31 @@
+package com.http.testsign;
+
+import sun.net.www.http.HttpClient;
+
+public class SignatureConsumer {
+	
+	private static String providerPbulicKey="MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCvvHPt7GXoh9Xl6hHzyyYdSPsx+NF/GS83q5ZR"
+			+"NJuUsCX7ljSnDdNl7VdHxL5D+wi1wU9NCwsW2s6uxbPRYX3azrd/NbKxWwAN/lSJAdbIrUgUKiga"
+			+"RG+PiH5y2gpGVec2NaL9gcsJjR+eGE12Aayetg/DSHE3ofHa73BP5JKcoQIDAQAB";
+	
+	private static String privateKey = "MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAIo0fZ5RqDxckOZsO7a0/6vPd7wv"
+			+"+LV9j2WZJyu9QIhFgNcUwgKem6LCiAjh6BHVRdJYQmv6gUZSKOqPgj9czcdKWeMT3VrsZsNlgQ49"
+			+"okeLrpypYUJb25AXYfRyBSsxpUQ2M69TwqK2pMw/j7QMiO4aL8ZR21WEW9IX39X4Vw1/AgMBAAEC"
+			+"gYBLcMe5ah2JhYXbIe2emEKPyJo1cYIzn6xs6yMErzPQrw/bWAtnXqjaOZIENzx0r5SDc4YeZwm2"
+			+"FxcaoueYKtJwT2O5X49vVsLj3NAR1/I5rsEG4vR0i/LeC9jAhDMbITbro1Dm9Ffpa529nLsIHsTb"
+			+"nQVO6nQCMa1tVMZ/DDeNsQJBAMIUgJoqGofAA7HHQzdIu921RGBlXxFeu+hrlZBrHPX+ZZtCGSAr"
+			+"evabOLPMJpfa+guHlKMum5r1jI+horWuxpUCQQC2TGRiTvmIX2YRU8HzahfQ/ZV0e6e/9xx39mk1"
+			+"L91l30DFIb6yHgUEpCNOrOP1mHuFmRhlBKeqg9VH+6Z+/iLDAkBNHsyDdhuYuco+CIpDG5nbXb5P"
+			+"97gxNM3Ca43BF28oqPBDI57MU7rG56M+fu8PBKq7jiKYPVsD1nMGWBpJy6uJAkBn9nL9OarHIhi0"
+			+"Yv7mEagZcLTOJEMLkCK1K1rekTiDA2oNwmvdao9UEUNtd93dTyOKkj++RIRWEy8MCTDvlYE/AkBf"
+			+"Turt4iZ3Pp96azHnYitm44KUtvdhDd5tYmgvVSgg1D5CrcKWwXU+G/gAvk6Ef+B9gUrxsCO+kCgz"
+			+"sLEe2yZO";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		HttpClient client = new 
+
+	}
+
+}
