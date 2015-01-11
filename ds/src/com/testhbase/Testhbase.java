866ee6ddbc87b704534058018a390e16a2ae563e
gitccsand <gozilla@163.com>
2015-01-11 20:49:41 +0800

    ds 6th week: hbase redis activemq

--- ds/src/com/testhbase/Testhbase.java
+++ ds/src/com/testhbase/Testhbase.java
@@ -25,7 +25,7 @@ public class Testhbase {
     static Configuration conf = null;
     static {
         conf =  HBaseConfiguration.create();
-        conf.set("hbase.zookeeper.quorum", "Centos");
+        conf.set("hbase.zookeeper.quorum", "centos");
     }
 
     /*
@@ -87,59 +87,7 @@ public class Testhbase {
     	table.put(put);
     	System.out.println(tableName + " "+ columnFamily + " add data Success!");
     }
-//
-//    /*
-//     * 根据rwokey查询
-//     * 
-//     * @rowKey rowKey
-//     * 
-//     * @tableName 表名
-//     */
-//    public static Result getResult(String tableName, String rowKey)
-//            throws IOException {
-//        Get get = new Get(Bytes.toBytes(rowKey));
-//        HTable table = new HTable(conf, Bytes.toBytes(tableName));// 获取表
-//        Result result = table.get(get);
-//        for (KeyValue kv : result.list()) {
-//            System.out.print("family:" + Bytes.toString(kv.getFamily()));
-//            System.out
-//                    .print("  qualifier:" + Bytes.toString(kv.getQualifier()));
-//            System.out.print("  value:" + Bytes.toString(kv.getValue()));
-//            System.out.println("  Timestamp:" + kv.getTimestamp());
-//        }
-//        System.out.println("-------------------------------------------");
-//        return result;
-//    }
-//
-//    /*
-//     * 遍历查询hbase表
-//     * 
-//     * @tableName 表名
-//     */
-//    public static void getResultScann(String tableName) throws IOException {
-//        Scan scan = new Scan();
-//        ResultScanner rs = null;
-//        HTable table = new HTable(conf, Bytes.toBytes(tableName));
-//        try {
-//            rs = table.getScanner(scan);
-//            for (Result r : rs) {
-//                for (KeyValue kv : r.list()) {
-//                    System.out.println("row:" + Bytes.toString(kv.getRow()));
-//                    System.out.println("family:"
-//                            + Bytes.toString(kv.getFamily()));
-//                    System.out.println("qualifier:"
-//                            + Bytes.toString(kv.getQualifier()));
-//                    System.out
-//                            .println("value:" + Bytes.toString(kv.getValue()));
-//                    System.out.println("timestamp:" + kv.getTimestamp());
-//                    System.out
-//                            .println("-------------------------------------------");
-//                }
-//            }
-//        } finally {
-//            rs.close();
-//        }
-//    }
+
 //
 //    /*
 //     * 遍历查询hbase表的rowkey范围
@@ -171,29 +119,7 @@ public class Testhbase {
             rs.close();
         }
     }
-//
-//    /*
-//     * 查询表中的某一列
-//     * 
-//     * @tableName 表名
-//     * 
-//     * @rowKey rowKey
-//     */
-//    public static void getResultByColumn(String tableName, String rowKey,
-//            String familyName, String columnName) throws IOException {
-//        HTable table = new HTable(conf, Bytes.toBytes(tableName));
-//        Get get = new Get(Bytes.toBytes(rowKey));
-//        get.addColumn(Bytes.toBytes(familyName), Bytes.toBytes(columnName)); // 获取指定列族和列修饰符对应的列
-//        Result result = table.get(get);
-//        for (KeyValue kv : result.list()) {
-//            System.out.println("family:" + Bytes.toString(kv.getFamily()));
-//            System.out
-//                    .println("qualifier:" + Bytes.toString(kv.getQualifier()));
-//            System.out.println("value:" + Bytes.toString(kv.getValue()));
-//            System.out.println("Timestamp:" + kv.getTimestamp());
-//            System.out.println("-------------------------------------------");
-//        }
-//    }
+
     
     /*
      * 查询表中的某一列族
@@ -219,63 +145,6 @@ public class Testhbase {
         }
         System.out.println("-------------------------------------------");
     }
-//
-//    /*
-//     * 更新表中的某一列
-//     * 
-//     * @tableName 表名
-//     * 
-//     * @rowKey rowKey
-//     * 
-//     * @familyName 列族名
-//     * 
-//     * @columnName 列名
-//     * 
-//     * @value 更新后的值
-//     */
-//    public static void updateTable(String tableName, String rowKey,
-//            String familyName, String columnName, String value)
-//            throws IOException {
-//        HTable table = new HTable(conf, Bytes.toBytes(tableName));
-//        Put put = new Put(Bytes.toBytes(rowKey));
-//        put.add(Bytes.toBytes(familyName), Bytes.toBytes(columnName),
-//                Bytes.toBytes(value));
-//        table.put(put);
-//        System.out.println("update table Success!");
-//    }
-//
-//    /*
-//     * 查询某列数据的多个版本
-//     * 
-//     * @tableName 表名
-//     * 
-//     * @rowKey rowKey
-//     * 
-//     * @familyName 列族名
-//     * 
-//     * @columnName 列名
-//     */
-//    public static void getResultByVersion(String tableName, String rowKey,
-//            String familyName, String columnName) throws IOException {
-//        HTable table = new HTable(conf, Bytes.toBytes(tableName));
-//        Get get = new Get(Bytes.toBytes(rowKey));
-//        get.addColumn(Bytes.toBytes(familyName), Bytes.toBytes(columnName));
-//        get.setMaxVersions(5);
-//        Result result = table.get(get);
-//        for (KeyValue kv : result.list()) {
-//            System.out.println("family:" + Bytes.toString(kv.getFamily()));
-//            System.out
-//                    .println("qualifier:" + Bytes.toString(kv.getQualifier()));
-//            System.out.println("value:" + Bytes.toString(kv.getValue()));
-//            System.out.println("Timestamp:" + kv.getTimestamp());
-//            System.out.println("-------------------------------------------");
-//        }
-        /*
-         * List<?> results = table.get(get).list(); Iterator<?> it =
-         * results.iterator(); while (it.hasNext()) {
-         * System.out.println(it.next().toString()); }
-         */
-//    }
 //
 //    /*
 //     * 删除指定的列
@@ -330,15 +199,8 @@ public class Testhbase {
         String tableName = "default:cards2";
         String[] family = { "tradeInfo", "customerInfo" };
         
-        // 删除表
-        deleteTable(tableName); 
         // 创建表
         creatTable(tableName, family);    
-         
-        // 删除所有列
-        deleteAllColumn("default:cards2", "tom");
-        deleteAllColumn("default:cards2", "jerry");
-        deleteAllColumn("default:cards2", "lisi");
 
         // 为表添加数据
         String customerFamily = "customerInfo";
@@ -347,7 +209,7 @@ public class Testhbase {
         String[] value2 = { "jerry", "89794654" };
         String[] value3 = { "lisi", "4564131" };
         
-        addData("tom", tableName, customerFamily, column1, value1);
+        addData("tom", tableName, customerFamily, column1, value1);//rowkey table colfamily cols values
         addData("jerry", tableName, customerFamily,column1, value2);
         addData("lisi", tableName, customerFamily,column1, value3);
         
@@ -357,13 +219,14 @@ public class Testhbase {
         String[] value2_2 = {"save","1000","10" };
         String[] value2_3 = {"save","10000","100" };
         
-        addData("tom", tableName, tradeFamily, column2, value2_1);
+        addData("tom", tableName, tradeFamily, column2, value2_1);//rowkey table colfamily cols values
         addData("tom", tableName, tradeFamily, column2, value2_2);
         addData("tom", tableName, tradeFamily ,column2, value2_3);
         
+        //遍历查询tom的数据   
         String start_rowkey = "tom";
         String end_rowkey = "tom";
-        getResultScann(tableName, start_rowkey, end_rowkey); //遍历查询tom的数据   
+        getResultScann(tableName, start_rowkey, end_rowkey);
         
         //修改tom的trade数据 增加dealer列
         getResultByColumnFamily(tableName, "tom", tradeFamily);//查询tom的trade数据   
@@ -374,46 +237,183 @@ public class Testhbase {
         deleteColumn(tableName, "tom", tradeFamily, "dealer");
         getResultByColumnFamily(tableName, "tom", tradeFamily);//查询tom的trade数据   
         
+      // 删除所有列
+      deleteAllColumn("default:cards2", "tom");
+      deleteAllColumn("default:cards2", "jerry");
+      deleteAllColumn("default:cards2", "lisi");
+      // 删除表
+      deleteTable(tableName); 
         
-        
-       //查询tom的trade:score数据的多个版本
-//        getResultByVersion(tableName, "tom", "trade", "score");
-//
-//        // 遍历查询
-//        getResultScann("blog2", "rowkey4", "rowkey5");
-//        // 根据row key范围遍历查询
-//        getResultScann("blog2", "rowkey4", "rowkey5");
-//
-//        // 查询某一列的值
-//        getResultByColumn("blog2", "rowkey1", "author", "name");
-//
-//        // 更新列
-//        updateTable("blog2", "rowkey1", "author", "name", "bin");
-//
-//        // 查询某一列的值
-//        getResultByColumn("blog2", "rowkey1", "author", "name");
-//
-//        // 查询某列的多版本
-//        getResultByVersion("blog2", "rowkey1", "author", "name");
-//
-//
-//
-//        // 删除表
-//        deleteTable("blog2");
 
     }
 
-
-
-//	private static void deleteColumn(String string, String string2,
-//			String string3, String string4) {
-//		// TODO Auto-generated method stub
-//		
-//	}
-
-
-
 }
+
+//查询tom的trade:score数据的多个版本
+//getResultByVersion(tableName, "tom", "trade", "score");
+//
+//// 遍历查询
+//getResultScann("blog2", "rowkey4", "rowkey5");
+//// 根据row key范围遍历查询
+//getResultScann("blog2", "rowkey4", "rowkey5");
+//
+//// 查询某一列的值
+//getResultByColumn("blog2", "rowkey1", "author", "name");
+//
+//// 更新列
+//updateTable("blog2", "rowkey1", "author", "name", "bin");
+//
+//// 查询某一列的值
+//getResultByColumn("blog2", "rowkey1", "author", "name");
+//
+//// 查询某列的多版本
+//getResultByVersion("blog2", "rowkey1", "author", "name");
+//
+//
+//
+//// 删除表
+//deleteTable("blog2");
+
+
+//private static void deleteColumn(String string, String string2,
+//	String string3, String string4) {
+//// TODO Auto-generated method stub
+//
+//}
+//
+///*
+//* 根据rwokey查询
+//* 
+//* @rowKey rowKey
+//* 
+//* @tableName 表名
+//*/
+//public static Result getResult(String tableName, String rowKey)
+//      throws IOException {
+//  Get get = new Get(Bytes.toBytes(rowKey));
+//  HTable table = new HTable(conf, Bytes.toBytes(tableName));// 获取表
+//  Result result = table.get(get);
+//  for (KeyValue kv : result.list()) {
+//      System.out.print("family:" + Bytes.toString(kv.getFamily()));
+//      System.out
+//              .print("  qualifier:" + Bytes.toString(kv.getQualifier()));
+//      System.out.print("  value:" + Bytes.toString(kv.getValue()));
+//      System.out.println("  Timestamp:" + kv.getTimestamp());
+//  }
+//  System.out.println("-------------------------------------------");
+//  return result;
+//}
+//
+///*
+//* 遍历查询hbase表
+//* 
+//* @tableName 表名
+//*/
+//public static void getResultScann(String tableName) throws IOException {
+//  Scan scan = new Scan();
+//  ResultScanner rs = null;
+//  HTable table = new HTable(conf, Bytes.toBytes(tableName));
+//  try {
+//      rs = table.getScanner(scan);
+//      for (Result r : rs) {
+//          for (KeyValue kv : r.list()) {
+//              System.out.println("row:" + Bytes.toString(kv.getRow()));
+//              System.out.println("family:"
+//                      + Bytes.toString(kv.getFamily()));
+//              System.out.println("qualifier:"
+//                      + Bytes.toString(kv.getQualifier()));
+//              System.out
+//                      .println("value:" + Bytes.toString(kv.getValue()));
+//              System.out.println("timestamp:" + kv.getTimestamp());
+//              System.out
+//                      .println("-------------------------------------------");
+//          }
+//      }
+//  } finally {
+//      rs.close();
+//  }
+//}
+
+//
+///*
+//* 查询表中的某一列
+//* 
+//* @tableName 表名
+//* 
+//* @rowKey rowKey
+//*/
+//public static void getResultByColumn(String tableName, String rowKey,
+//      String familyName, String columnName) throws IOException {
+//  HTable table = new HTable(conf, Bytes.toBytes(tableName));
+//  Get get = new Get(Bytes.toBytes(rowKey));
+//  get.addColumn(Bytes.toBytes(familyName), Bytes.toBytes(columnName)); // 获取指定列族和列修饰符对应的列
+//  Result result = table.get(get);
+//  for (KeyValue kv : result.list()) {
+//      System.out.println("family:" + Bytes.toString(kv.getFamily()));
+//      System.out
+//              .println("qualifier:" + Bytes.toString(kv.getQualifier()));
+//      System.out.println("value:" + Bytes.toString(kv.getValue()));
+//      System.out.println("Timestamp:" + kv.getTimestamp());
+//      System.out.println("-------------------------------------------");
+//  }
+//}
+//
+///*
+//* 更新表中的某一列
+//* 
+//* @tableName 表名
+//* 
+//* @rowKey rowKey
+//* 
+//* @familyName 列族名
+//* 
+//* @columnName 列名
+//* 
+//* @value 更新后的值
+//*/
+//public static void updateTable(String tableName, String rowKey,
+//      String familyName, String columnName, String value)
+//      throws IOException {
+//  HTable table = new HTable(conf, Bytes.toBytes(tableName));
+//  Put put = new Put(Bytes.toBytes(rowKey));
+//  put.add(Bytes.toBytes(familyName), Bytes.toBytes(columnName),
+//          Bytes.toBytes(value));
+//  table.put(put);
+//  System.out.println("update table Success!");
+//}
+//
+///*
+//* 查询某列数据的多个版本
+//* 
+//* @tableName 表名
+//* 
+//* @rowKey rowKey
+//* 
+//* @familyName 列族名
+//* 
+//* @columnName 列名
+//*/
+//public static void getResultByVersion(String tableName, String rowKey,
+//      String familyName, String columnName) throws IOException {
+//  HTable table = new HTable(conf, Bytes.toBytes(tableName));
+//  Get get = new Get(Bytes.toBytes(rowKey));
+//  get.addColumn(Bytes.toBytes(familyName), Bytes.toBytes(columnName));
+//  get.setMaxVersions(5);
+//  Result result = table.get(get);
+//  for (KeyValue kv : result.list()) {
+//      System.out.println("family:" + Bytes.toString(kv.getFamily()));
+//      System.out
+//              .println("qualifier:" + Bytes.toString(kv.getQualifier()));
+//      System.out.println("value:" + Bytes.toString(kv.getValue()));
+//      System.out.println("Timestamp:" + kv.getTimestamp());
+//      System.out.println("-------------------------------------------");
+//  }
+/*
+* List<?> results = table.get(get).list(); Iterator<?> it =
+* results.iterator(); while (it.hasNext()) {
+* System.out.println(it.next().toString()); }
+*/
+//}
 
 /*
  * 为表添加数据（适合知道有多少列族的固定表）
