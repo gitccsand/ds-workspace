14cc89f140245cb2e88019b6587046f424056061
gitccsand <gozilla@163.com>
2015-01-22 16:59:45 +0800

    alg projiect and arange sort and lib

---
+++ testcpp/src/testcpp.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : testcpp.cpp
+// Author      : lhw
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <queue>
+#include <string>
+#include <map>
+using namespace std;
+
+queue<long> nque[1000];
+queue<int> ns[2];
+
+int main() {
+	cout << "Hello World!!!" << endl; // prints Hello World!!!
+	return 0;
+}
